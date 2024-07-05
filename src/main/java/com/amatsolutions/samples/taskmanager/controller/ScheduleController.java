4b1c091eafbc9b939acb0460a54114fd2935f6af
Albin Amat <aamatreyes@gmail.com>
2024-07-04 22:04:41 -0400

    added static schedule

---
+++ src/main/java/com/amatsolutions/samples/taskmanager/controller/ScheduleController.java
@@ -0,0 +1,13 @@
+package com.amatsolutions.samples.taskmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ScheduleController {
+
+    @GetMapping("/schedule")
+    public String schedule() {
+        return "forward:/schedule/index.html";
+    }
+}
